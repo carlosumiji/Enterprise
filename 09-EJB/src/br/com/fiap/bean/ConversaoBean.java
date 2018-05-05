fa0fc1c97916a0b649725cef470671571473b29b
logonrm <logonrm@fiap.com.br>
2018-05-04 22:45:24 -0300

    criado o projeto 09-EJB

---
+++ 09-EJB/src/br/com/fiap/bean/ConversaoBean.java
@@ -0,0 +1,34 @@
+package br.com.fiap.bean;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import br.com.fiap.bo.ConversaoBO;
+
+@ManagedBean
+public class ConversaoBean {
+	private double valor;
+	private double resultado;
+	
+	@EJB
+	private ConversaoBO bo;
+	
+	public void converter() {
+		resultado = bo.converterDolarReal(valor);
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public double getResultado() {
+		return resultado;
+	}
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+	
+	
+}
